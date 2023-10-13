48c46ccd67ca76847d1dc02e3fd1513f95b080c4
focusotter <mtliendo@focusotter.com>
2023-10-12 21:13:20 -0500

    init wordsearch generator for the wifey

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,134 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState } from 'react'
+import WordSearch from '@blex41/word-search'
 
-const inter = Inter({ subsets: ['latin'] })
+function WordSearchApp() {
+	const [cols, setCols] = useState(6)
+	const [rows, setRows] = useState(6)
+	const [words, setWords] = useState('')
+	const [wordBank, setWordBank] = useState([])
 
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [grid, setGrid] = useState([])
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const handleGenerate = () => {
+		const dictionary = words.split(',').map((word) => word.trim())
+		const options = {
+			cols,
+			rows,
+			disabledDirections: [],
+			dictionary,
+			upperCase: true,
+		}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		const ws = new WordSearch(options)
+		const pickedWords = ws.data.words.map((word) => word.clean)
+		setGrid(ws.data.grid)
+		setWordBank(pickedWords)
+	}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	return (
+		<div className="p-4 md:p-8 bg-gray-100 min-h-screen">
+			<div className="max-w-xl mx-auto">
+				<section className="hideable">
+					{/* Grid Size Selection */}
+					<div className="flex flex-wrap -mx-2 mb-4">
+						<div className="w-1/2 px-2">
+							<label className="block text-gray-700 mb-2">
+								Columns:
+								<input
+									type="number"
+									value={cols}
+									onChange={(e) => setCols(Number(e.target.value))}
+									className="w-full mt-1 p-2 border rounded"
+								/>
+							</label>
+						</div>
+						<div className="w-1/2 px-2">
+							<label className="block text-gray-700 mb-2">
+								Rows:
+								<input
+									type="number"
+									value={rows}
+									onChange={(e) => setRows(Number(e.target.value))}
+									className="w-full mt-1 p-2 border rounded"
+								/>
+							</label>
+						</div>
+					</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+					{/* Word Input */}
+					<div className="mb-4">
+						<label className="block text-gray-700 mb-2">
+							Words (comma-separated):
+							<textarea
+								value={words}
+								onChange={(e) => setWords(e.target.value)}
+								rows={4}
+								placeholder="Enter words separated by commas."
+								className="w-full mt-1 p-2 border rounded"
+							></textarea>
+						</label>
+					</div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+					{/* Generate Button */}
+					<div className="mb-4">
+						<button
+							onClick={handleGenerate}
+							className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+						>
+							Generate Grid
+						</button>
+					</div>
+				</section>
+				{/* Display Grid */}
+				{grid.length > 0 && (
+					<div className="printable-grid bg-white p-4 rounded shadow">
+						<table className="w-full text-center">
+							<tbody>
+								{grid.map((row, rowIndex) => (
+									<tr key={rowIndex}>
+										{row.map((cell, cellIndex) => (
+											<td key={cellIndex} className="border p-2">
+												{cell}
+											</td>
+										))}
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+				)}
+
+				{/* ... rest of your component code ... */}
+
+				{/* Word Bank */}
+				<div className="mt-6">
+					<h2 className="text-xl font-semibold mb-4 text-gray-700">
+						Word Bank:
+					</h2>
+					<div className="flex flex-wrap">
+						{wordBank.map((word, index) => (
+							<span
+								key={index}
+								className="m-1 p-2 bg-white rounded shadow text-gray-700"
+							>
+								{word}
+							</span>
+						))}
+					</div>
+				</div>
+
+				{/* Print Button */}
+				<div className="hideable mt-4">
+					<button
+						onClick={() => window.print()}
+						className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+					>
+						Print Grid
+					</button>
+				</div>
+			</div>
+		</div>
+	)
 }
+
+export default WordSearchApp
